d6d1141a2bf1b8bc94f06dd02c5a844e314a6abe
Aris Giotis <a.giotis@NUK41003391.scientificgames.com>
2024-04-24 19:37:04 +0300

    Listening to multiple observables with combineLatestWith

--- 04_01/Starter Code/src/app/app.component.ts
+++ 04_01/Starter Code/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// step 1
+import { Subject } from 'rxjs';
+// step 2
+import { combineLatestWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,15 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  // step 3
+  temperatureSubject$ = new Subject<number>();
+  // step 4
+  feelsLikeSubject$ = new Subject<number>();
   temperatureDataList: number[] = [];
   inputTemperature = 0;
   inputFeelsLikeTemperature = 0;
   displayText = '';
 
   ngOnInit() {
+    // step 7
+    this.temperatureSubject$
+    .pipe(combineLatestWith(this.feelsLikeSubject$))
+    .subscribe(([temperature, feelsLikeTemperature]) => {
+      this.displayText = `It's ${temperature} F, but it feels like ${feelsLikeTemperature} F`;
+    })
   }
 
   setTemperature() {
+    // step 5
+    this.temperatureSubject$.next(this.inputTemperature);
   }
 
   setInputTemperature(event: Event) {
@@ -23,6 +39,8 @@ export class AppComponent implements OnInit {
   }
 
   setFeelsLike() {
+    // step 6
+    this.feelsLikeSubject$.next(this.inputFeelsLikeTemperature);
   }
 
   setInputFeelsLike(event: Event) {
