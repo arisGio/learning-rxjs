df272d0212d9dd50cc60e72df23db2080c389c31
Aris Giotis <a.giotis@NUK41003391.scientificgames.com>
2024-04-24 19:48:09 +0300

    4.2 Preventing repeated calls using exhaustMap

--- 04_02/Starter Code/src/app/app.component.ts
+++ 04_02/Starter Code/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
+// step 1
+import { from } from 'rxjs';
+// step 2
+import { exhaustMap, delay, toArray } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,10 +19,23 @@ export class AppComponent implements OnInit {
   attemptedCallCount = 0;
 
   ngOnInit() {
+    // step 4
+    this.temperatureSubject$
+    .pipe(exhaustMap((temperatureDataList) => {
+      // step 6
+      return from(temperatureDataList).pipe(delay(1000), toArray());
+    }))
+    .subscribe((temperatureDataList) => {
+      this.temperatureDataList = temperatureDataList;
+      // step 5
+      this.callCount = this.callCount + 1;
+    })
   }
 
   getWeather() {
     this.attemptedCallCount = this.attemptedCallCount + 1;
     const temperatureDataList = [51, 73, 64, 21];
+    // step 3    
+    this.temperatureSubject$.next(temperatureDataList);
   }
 }
